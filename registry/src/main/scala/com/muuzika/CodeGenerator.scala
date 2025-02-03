9269e391404871793e50e6dd7e1d53e881580283
Levy Barbosa <marcio.levy20@gmail.com>
2025-02-02 23:02:16 -0300

    fix warning

--- registry/src/main/scala/com/muuzika/CodeGenerator.scala
+++ registry/src/main/scala/com/muuzika/CodeGenerator.scala
@@ -5,16 +5,16 @@ import com.muuzika.common.RoomCode
 import scala.collection.mutable
 import scala.util.Random
 
-import scala.collection.mutable
-import scala.util.Random
-
 class CodeCollection(rng: Random, min: Int, val power: Int) {
-  private val codes: mutable.Queue[Int] = mutable.Queue((min until math.pow(10, power + 1).toInt): _*)
+  private val codes: mutable.Queue[Int] = mutable.Queue(min until math.pow(10, power + 1).toInt: _*)
   rng.shuffle(codes)
 
   def isEmpty: Boolean = codes.isEmpty
+
   def available: Int = codes.length
+
   def popFront(): Option[Int] = if (codes.nonEmpty) Some(codes.dequeue()) else None
+
   def pushBack(code: Int): Unit = codes.enqueue(code)
 }
 
@@ -24,6 +24,12 @@ class CodeGenerator(seed: Option[Long], initialPower: Int, capacityThresholdToRe
 
   collections.append(new CodeCollection(rng, 1, initialPower))
 
+  def getCode: (RoomCode, Boolean) = this.synchronized {
+    val collection = collections.find(!_.isEmpty).getOrElse(createNextCollection)
+    val code = RoomCode(collection.popFront().get)
+    (code, isEmpty)
+  }
+
   def isEmpty: Boolean = collections.forall(_.isEmpty)
 
   def createNextCollection: CodeCollection = this.synchronized {
@@ -31,12 +37,6 @@ class CodeGenerator(seed: Option[Long], initialPower: Int, capacityThresholdToRe
     val collection = new CodeCollection(rng, math.pow(10, power).toInt, power)
     collections.append(collection)
     collection
-  }
-
-  def getCode: (RoomCode, Boolean) = this.synchronized {
-    val collection = collections.find(!_.isEmpty).getOrElse(createNextCollection)
-    val code = RoomCode(collection.popFront().get)
-    (code, isEmpty)
   }
 
   def returnCode(code: RoomCode): Unit = this.synchronized {
