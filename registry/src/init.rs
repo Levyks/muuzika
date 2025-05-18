efe30276507591274534670d03e7146fdd15f3a4
Levy Barbosa <marcio.levy20@gmail.com>
2025-05-18 17:33:40 -0300

    refactored server startup for tests

--- registry/src/init.rs
+++ registry/src/init.rs
@@ -1,37 +1,48 @@
-use std::net::SocketAddr;
-use std::sync::Arc;
-use tokio::sync::oneshot::Receiver;
-use tokio::sync::RwLock;
-use tonic::transport::Server;
 use crate::generator::{RoomCodeCollection, RoomCodeGeneratorImpl};
 use crate::options::Options;
 use crate::registry::Registry;
 use crate::services::RegistryGrpcServices;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::net::TcpListener;
+use tokio::sync::oneshot::Receiver;
+use tokio::sync::RwLock;
+use tonic::codegen::tokio_stream::wrappers::TcpListenerStream;
+use tonic::transport::Server;
 
-async fn serve_base(listen_address: SocketAddr, options: Options, generator: Option<RoomCodeGeneratorImpl>, shutdown_rx: Option<Receiver<()>>) {
+#[derive(Debug)]
+enum ServeOpts {
+    Addr(SocketAddr),
+    ListenerAndShutdown(TcpListener, Receiver<()>),
+}
+
+async fn serve_base(opts: ServeOpts, options: Options, generator: Option<RoomCodeGeneratorImpl>) {
     let generator = generator.unwrap_or_else(|| RoomCodeGeneratorImpl::new(None, 3, 200));
     let registry = Arc::new(RwLock::new(Registry::new(generator)));
-    log::info!("Starting registry server on {listen_address} with options: {options:?}");
+    log::info!("Starting registry server on {opts:?} with options: {options:?}");
 
     let router = Server::builder().add_registry_services(registry, options);
 
-    let result = if let Some(shutdown_rx) = shutdown_rx {
-        router.serve_with_shutdown(listen_address, async {
-            shutdown_rx.await.ok();
-        }).await
-    } else {
-        router.serve(listen_address).await
+    let result = match opts {
+        ServeOpts::Addr(listen_address) => {
+            router.serve(listen_address).await
+        }
+        ServeOpts::ListenerAndShutdown(listener, shutdown_rx) => {
+            router.serve_with_incoming_shutdown(TcpListenerStream::new(listener), async {
+                shutdown_rx.await.ok();
+            }).await
+        }
     };
 
     result.expect("Failed to start server");
 }
 
 pub async fn serve(listen_address: SocketAddr, options: Options) {
-    serve_base(listen_address, options, None, None).await;
+    serve_base(ServeOpts::Addr(listen_address), options, None).await
 }
 
 pub async fn serve_with_shutdown_and_codes(
-    listen_address: SocketAddr,
+    listener: TcpListener,
     options: Options,
     shutdown_rx: Receiver<()>,
     codes: Option<Vec<u32>>,
@@ -41,5 +52,9 @@ pub async fn serve_with_shutdown_and_codes(
         let code_collection = RoomCodeCollection::new_from_vec(power, codes);
         RoomCodeGeneratorImpl::new_with_collection(code_collection, power as usize)
     });
-    serve_base(listen_address, options, generator, Some(shutdown_rx)).await;
+    serve_base(
+        ServeOpts::ListenerAndShutdown(listener, shutdown_rx),
+        options,
+        generator,
+    ).await
 }
